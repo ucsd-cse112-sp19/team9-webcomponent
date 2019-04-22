efd4519814d0d8f6df5bee5edd12c797fe947e43
Kevin Zavier <kzavier@ucsd.edu>
2019-04-22 16:55:39 -0700

    sample web component

---
+++ test.js
@@ -0,0 +1,22 @@
+class HelloWorld extends HTMLElement {
+  // Can define constructor arguments if you wish.
+  constructor() {
+    // If you define a constructor, always call super() first!
+    // This is specific to CE and required by the spec.
+    super();
+    console.log("Created")
+    this.clicks = 0;
+    // Setup a click listener on <app-drawer> itself.
+    this.addEventListener('click', e => {
+      this.drawer();
+    });
+  }
+
+  drawer() {
+    this.clicks+=1;
+
+    document.getElementById("example").innerHTML = "Click counter by jordan:" + "<br />" + this.clicks;
+  }
+}
+
+window.customElements.define('hello-world', HelloWorld);
