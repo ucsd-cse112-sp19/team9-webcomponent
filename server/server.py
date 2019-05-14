b9060037304a46e94584a17ff29264b139f162a0
Alex Haggart <ahaggart@ucsd.edu>
2019-05-14 14:56:26 -0700

    multipart stream POC

--- server/server.py
+++ server/server.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import json
+import random
+import time
 
-from flask import Flask
+from flask import Flask, Response
 app = Flask(__name__)
 
 
@@ -12,11 +14,29 @@ def hello():
 
 @app.route("/messages")
 def messages():
-    return json.dumps([
-        "hello there :^)",
-        "these are fake messages!",
-        "hopefully the dev server works",
-    ])
+    boundary = "frame"
+    return Response(
+        random_msg_stream(boundary),
+        mimetype='multipart/x-mixed-replace; boundary={}'.format(boundary)
+    )
+
+
+def random_msg_stream(boundary):
+    boundary = "--{}".format(boundary)
+    for _ in xrange(0, random.randint(1, 30)):
+        time.sleep(0.5)
+        msg = random_msg()
+        yield "\r\n".join([
+            boundary,
+            "Content-Length: {}".format(len(msg)),
+            "\r\n",
+        ]).encode('utf-8')
+        yield msg
+
+
+def random_msg():
+    length = random.randint(5, 25)
+    return str.join('', [str(chr(random.randint(65, 90))) for _ in xrange(0, length)])
 
 
 if __name__ == "__main__":
