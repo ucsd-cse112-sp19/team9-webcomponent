bf3656986153cb0e4cf56bd31bb273c16e289822
Eric Sen <esen@ucsd.edu>
2019-05-16 23:15:22 -0700

    adding some comments

--- web-component/mqttFetch.js
+++ web-component/mqttFetch.js
@@ -24,7 +24,11 @@ class MqttFetch extends HTMLElement {
         this.removeAttribute('url');
       }
     }
-
+    /**
+     * get topic() 
+     * Check if topic exists in HTML.
+     * Returns: True or False 
+     */
     get topic() {
         return this.getAttribute('topic');
     }
@@ -45,6 +49,8 @@ class MqttFetch extends HTMLElement {
       // Initialize shadow root
       const shadowRoot = this.attachShadow({mode: 'open'});
 
+      // TODO: This should be optimized or another method should be implemented.
+      // This is a helper function that generates a random client id.
       function makeid(length) {
         let result           = '';
         const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -59,6 +65,7 @@ class MqttFetch extends HTMLElement {
       // NOTE: If you have client issues its because there is another client connected with this name
       // Choose another random client id and it should fix the problem
       // You can determine this by opening dev tools and if it says socket closed that is the problem
+      // TODO: FIX ABOVE -- kinda fixed but perhaps a better fix can be implemented
       this.client = new Paho.MQTT.Client("broker.mqttdashboard.com", Number(8000), makeid(8));
 
       this.client.onConnectionLost = function(responseObject){
