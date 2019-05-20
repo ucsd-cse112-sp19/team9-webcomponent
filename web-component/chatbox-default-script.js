853305c51f22907f10bdd0b979e6bbf151bdeda3
Kaung Yang <kky006@ucsd.edu>
2019-05-20 12:06:42 -0700

    cleand up css for chatbox and chatstream to have default css

--- web-component/chatbox-default-script.js
+++ web-component/chatbox-default-script.js
@@ -1,13 +1,13 @@
-const addChatBoxDefaultStyles = () => {
-    let el = document.querySelector('chat-box');
-    let id_el =el.shadowRoot.querySelector("#sender").shadowRoot.querySelector("#userId");
-    id_el.style.cssText = `
-    color: #006A96; border: none; font-size: 15px;
-    box-sizing: border-box; border: 2px solid #685972;
-    width: 100%; height: 25px; border-bottom: none;
-    `;
-}
+// const addChatBoxDefaultStyles = () => {
+//     let el = document.querySelector('chat-box');
+//     let id_el =el.shadowRoot.querySelector("#sender").shadowRoot.querySelector("#userId");
+//     id_el.style.cssText = `
+//     color: #006A96; border: none; font-size: 15px;
+//     box-sizing: border-box; border: 2px solid #685972;
+//     width: 100%; height: 25px; border-bottom: none;
+//     `;
+// }
 
-addChatBoxDefaultStyles(); 
+// addChatBoxDefaultStyles(); 
 
 
