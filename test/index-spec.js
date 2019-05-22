2f9da3f70c172672703853ecc84144b1209c7cb4
Kaung Yang <kky006@ucsd.edu>
2019-05-22 11:46:15 -0700

    unit testing started with olivia, started testing bootstrap

--- test/index-spec.js
+++ test/index-spec.js
@@ -1,8 +1,75 @@
 const expect = require('chai').expect
-const server = require('../index');
+const assert = require('chai').assert
+const jsdom = require('jsdom-global')();
+const doc = require('document-register-element');
 
-describe('test', () => {
-  it('should return a string', () => {
-    expect('ci with travis').to.equal('ci with travis');
+const inputBox = require('../web-component/inputBox.js');
+
+describe('input-box element', () => {
+  let cs;
+  before(() => {
+    
+  });
+
+  after(() => {
+
+  });
+
+  beforeEach(() => {
+    cs = document.createElement('input-box');
+    cs.connectedCallback();
+  });
+
+  it('creates element from document.createElement', () => {
+    assert.equal('INPUT-BOX', cs.nodeName);
+  });
+
+  it('should find input element in shadowDom', () => {
+    const input = cs.shadowRoot.querySelector('input');
+    assert.equal(input.getAttribute('id'), 'msg');
+    assert.equal(input.getAttribute('name'), 'msg');
+  });
+
+  describe(`bootstrap attribute test`, () => {
+    beforeEach(() => {
+      cs = document.createElement('input-box');
+      cs.connectedCallback();
+    });
+
+    it("should insert bootstrap classes into input's classes", () => {
+      const input = cs.shadowRoot.querySelector('input');
+      assert.equal(input.getAttribute('bootstrap'), 'msg');
+      assert.equal(input.getAttribute('name'), 'msg');
+    }); 
+  })
+});
+
+// unit test for output-stream TODO: remove when commiting 
+describe('output-stream element', () => {
+  let cs;
+  before(() => {
+
+  });
+
+  after(() => {
+
+  });
+
+  beforeEach(() => {
+    cs = document.createElement('output-stream');
+    cs.connectedCallback();
+  });
+
+  it('creates element from document.createElement', () => {
+    assert.equal('OUTPUT-STREAM', cs.nodeName);
+  });
+
+  it('should find input element in shadowDom', () => {
+    const input = cs.shadowRoot.querySelector('link');
+    assert.equal(input.getAttribute('rel'), 'stylesheet');
+    assert.equal(input.getAttribute('type'), 'text/css');
   });
 });
+
+
+
