f430af0f501862f0c083c27f316545b3df6c3120
Kaung Yang <kky006@ucsd.edu>
2019-05-24 13:52:54 -0700

    work in progress for porting input from element.eleme.io

--- test/index-spec.js
+++ test/index-spec.js
@@ -4,72 +4,47 @@ const jsdom = require('jsdom-global')();
 const doc = require('document-register-element');
 
 const inputBox = require('../web-component/inputBox.js');
+const outputStream = require('../web-component/outputStream.js');
 
 describe('input-box element', () => {
-  let cs;
-  before(() => {
-    
-  });
+  describe('element creation', () => {
+    let cs;
+    before(() => {
 
-  after(() => {
+    });
 
-  });
+    after(() => {
 
-  beforeEach(() => {
-    cs = document.createElement('input-box');
-    cs.connectedCallback();
-  });
-
-  it('creates element from document.createElement', () => {
-    assert.equal('INPUT-BOX', cs.nodeName);
-  });
-
-  it('should find input element in shadowDom', () => {
-    const input = cs.shadowRoot.querySelector('input');
-    assert.equal(input.getAttribute('id'), 'msg');
-    assert.equal(input.getAttribute('name'), 'msg');
-  });
-
-  describe(`bootstrap attribute test`, () => {
+    });
     beforeEach(() => {
       cs = document.createElement('input-box');
       cs.connectedCallback();
     });
-
-    it("should insert bootstrap classes into input's classes", () => {
+    it('creates element from document.createElement', () => {
+      assert.equal('INPUT-BOX', cs.nodeName);
+    });
+    it('should find input element in shadowDom', () => {
       const input = cs.shadowRoot.querySelector('input');
-      assert.equal(input.getAttribute('bootstrap'), 'msg');
+      assert.equal(input.getAttribute('id'), 'msg');
       assert.equal(input.getAttribute('name'), 'msg');
-    }); 
+    });
   })
-});
 
-// unit test for output-stream TODO: remove when commiting 
-describe('output-stream element', () => {
-  let cs;
-  before(() => {
+  describe("bootstrap attribute", function () {
+    let cs;
+    beforeEach(() => {
+      cs = document.createElement('input-box');
+      cs.setAttribute('url', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css');
+      cs.setAttribute('bootstrap', 'text-danger');
+      cs.connectedCallback();
+    });
+    it("checks if bootstrap text is set", function () {
+      const input = cs.shadowRoot.querySelector('input');
+      let text = input.getAttribute('class');
+      assert.equal(input.getAttribute('name'), 'msg');
+    });
 
-  });
-
-  after(() => {
-
-  });
-
-  beforeEach(() => {
-    cs = document.createElement('output-stream');
-    cs.connectedCallback();
-  });
-
-  it('creates element from document.createElement', () => {
-    assert.equal('OUTPUT-STREAM', cs.nodeName);
-  });
-
-  it('should find input element in shadowDom', () => {
-    const input = cs.shadowRoot.querySelector('link');
-    assert.equal(input.getAttribute('rel'), 'stylesheet');
-    assert.equal(input.getAttribute('type'), 'text/css');
   });
 });
-
 
 
