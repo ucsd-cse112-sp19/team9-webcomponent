b47821933273a07bbe20476405279db71e52a1af
Eric Sen <esen@ucsd.edu>
2019-05-23 18:18:27 -0700

    testing

---
+++ test/inputBox-spec.js
@@ -0,0 +1,34 @@
+const assert = require('chai').assert
+
+const jsdom = require('jsdom-global')();
+const doc = require('document-register-element');
+
+const inputBox = require('../web-component/inputBox');
+
+describe('input-box element', () => {
+    let cs;
+
+    before(() => {
+
+    });
+
+    after(() => {
+
+    });
+
+    beforeEach(() => {
+        cs = document.createElement('input-box');        
+        cs.connectedCallback();
+    });
+
+    it('creates element from document.createElement', () => {
+        assert.equal('INPUT-BOX', cs.nodeName); 
+    });
+
+    it('should find input element in shadowDom', () => {
+        const input = cs.shadowRoot.querySelector('input');
+        
+        assert.equal(input.getAttribute('id'), 'msg');
+        assert.equal(input.getAttribute('name'), 'msg');
+    });
+});
