df6c57eb157363a85be995b0d231d0257ce72758
Kaung Yang <kky006@ucsd.edu>
2019-05-27 16:26:16 -0700

    web component takes in bootstrap url through the url attribute; disabled function working

--- web-component/inputBox.js
+++ web-component/inputBox.js
@@ -3,6 +3,32 @@
  * Provides template for input element
  */
 class InputBox extends HTMLElement {
+
+
+
+    /**
+     * get disabled() 
+     * Check if disabled exists in HTML.
+     * Returns: True or False 
+     */
+    get disabled() {
+        return this.hasAttribute('disabled');
+    }
+    /**
+     * set disabled(val) 
+     * Sets disabled if value passed in, or removes it if nothing
+     * is passed.
+     * Returns: Null
+     */
+    set disabled(val) {
+        const isDisabled = Boolean(val);
+        if (isDisabled) {
+            this.setAttribute('disabled', val);
+        } else {
+            this.removeAttribute('disabled');
+        }
+    }
+
     /**
      * get bootstrap() 
      * Check if bootstrap exists in HTML.
@@ -22,6 +48,28 @@ class InputBox extends HTMLElement {
             this.setAttribute('bootstrap', val);
         } else {
             this.removeAttribute('bootstrap');
+        }
+    }
+
+    /**
+     * get url() 
+     * Check if url exists in HTML.
+     * Returns: True or False 
+     */
+    get url() {
+        return this.getAttribute('url');
+    }
+    /**
+     * set url(val) 
+     * Sets url if value passed in, or removes it if nothing
+     * is passed.
+     * Returns: Null
+     */
+    set url(val) {
+        if (val !== '') {
+            this.setAttribute('url', val);
+        } else {
+            this.removeAttribute('url');
         }
     }
     /**
@@ -53,13 +101,26 @@ class InputBox extends HTMLElement {
         i.setAttribute("value","");
         shadowRoot.append(i);
 
-        if (this.bootstrap) {
+        if (this.url && this.bootstrap) {
+            l.setAttribute('href', this.url);
             i.setAttribute('class', this.bootstrap);
-            l.setAttribute('href', './bootstrap.min.css');
+            
         } else {
             l.setAttribute('href', 'inputbox-default-style.css');
         } 
 
+        if (this.disabled) {
+            let disabledStyle = document.createElement('style'); 
+            disabledStyle.innerHTML += `input {
+                opacity: 0.5!important;
+                cursor: not-allowed;
+                background-color: #ccc;
+            }`; 
+            i.setAttribute('disabled', true)
+           
+            console.log(shadowRoot.querySelector('link'));
+            shadowRoot.querySelector('link').insertAdjacentElement("beforebegin", disabledStyle)
+        }  
         // TODO: We need to make a logical filter that can 
         // listen to some sort of backplane that could trigger this
         // from an internal button 
