8b7d1dc2c4eaea4bdf3e05a07ae2412f3e9f99c0
Valentin <valentin.bielecki@gmail.com>
2019-05-31 13:54:27 -0700

    New Unit Tests

--- test/inputBox-spec.js
+++ test/inputBox-spec.js
@@ -17,7 +17,6 @@ describe('input-box element', () => {
 
     beforeEach(() => {
         cs = document.createElement('input-box');        
-        cs.connectedCallback();
     });
 
     it('creates element from document.createElement', () => {
@@ -25,10 +24,94 @@ describe('input-box element', () => {
     });
 
     it('should find input element in shadowDom', () => {
+        cs.connectedCallback();
         const input = cs.shadowRoot.querySelector('input');
         
         assert.equal(input.getAttribute('id'), 'msg');
         assert.equal(input.getAttribute('name'), 'msg');
         assert.equal(input.getAttribute('value'), '');
     });
+    describe('set and delete all the attributes', () => {
+        it('should find the bootstrap',() => {
+            cs.setAttribute('boostrap','border border-secondary');
+            assert.equal(cs.getAttribute('boostrap'),'border border-secondary') ;
+        });
+
+        it('should delete the bootstrap',() => {
+            cs.bootstrap='';
+            assert.equal(cs.getAttribute('boostrap'), null);     
+        });
+
+        it('should set the bootstrap',() => {
+            cs.bootstrap='border border-secondary';
+            assert.equal(cs.bootstrap,'border border-secondary');     
+        });
+
+        it('should set the size',() => {
+            cs.size=1;
+            assert.equal(cs.getAttribute('size'), 1);
+        });  
+
+        it('should delete the size',() => {
+            cs.size='';
+            assert.equal(cs.getAttribute('size'), null);
+        }); 
+
+        it('should set the height',() => {
+            cs.height=1;
+            assert.equal(cs.getAttribute('height'), 1);
+        });  
+
+        it('should delete the height',() => {
+            cs.height='';
+            assert.equal(cs.getAttribute('height'), null);
+        }); 
+
+        it('should set the width',() => {
+            cs.width=1;
+            assert.equal(cs.getAttribute('width'), 1);
+        });  
+
+        it('should delete the width',() => {
+            cs.width='';
+            assert.equal(cs.getAttribute('width'), null);
+        }); 
+
+        it('should set the disabled',() => {
+            cs.disabled=true;
+            assert.equal(cs.getAttribute('disabled'), 'true');
+        });  
+
+        it('should delete the disabled ',() => {
+        cs.disabled=false;
+        assert.equal(cs.getAttribute('disabled'), null);
+        }); 
+
+        it('should set the url',() => {
+            cs.url='fake url';
+            assert.equal(cs.getAttribute('url'), 'fake url');
+        });  
+
+        it('should delete the url',() => {
+            cs.url='';
+            assert.equal(cs.getAttribute('url'), null);
+        }); 
+    });
+
+    it('should set the text area if we have bootstrap',() => {
+        cs.bootstrap='border border-secondary';
+        cs.url='fake u'
+        cs.connectedCallback();
+        const input = cs.shadowRoot.querySelector('input');
+        const link = cs.shadowRoot.querySelector('link');
+        assert.equal(input.getAttribute('class'),cs.bootstrap)
+        assert.equal(link.getAttribute('href'),cs.url);
+    });
+
+    it('should set the link by default if no bootstrap',() => {
+        cs.connectedCallback();
+        const link = cs.shadowRoot.querySelector('link');
+        assert.equal(link.getAttribute('href'),'inputbox-default-style.css');
+    });
+
 });
